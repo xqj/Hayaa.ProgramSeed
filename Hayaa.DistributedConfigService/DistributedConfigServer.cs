a390838391cfd7c744be8f55f92adf9eef9858c1
hsieh jing <xqj198404@gmail.com>
2016-08-28 00:23:58 +0800

    基础构建

---
+++ Hayaa.DistributedConfigService/DistributedConfigServer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hayaa.DistributedConfigService.Interface;
+
+namespace Hayaa.DistributedConfigService
+{
+    public class DistributedConfigServer: IDistributedConfigService
+    {
+    }
+}
