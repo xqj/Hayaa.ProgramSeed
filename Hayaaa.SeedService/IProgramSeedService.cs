9485a3eafe5187563f43f187c9c67806537e7211
xqj <xqj198404@gmail.com>
2016-12-13 18:18:48 +0800

    22

---
+++ Hayaaa.SeedService/IProgramSeedService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hayaaa.SeedService
+{
+  public  interface IProgramSeedService
+    {
+        /// <summary>
+        /// 哨兵服务，种子端发送的哨兵信息
+        /// </summary>
+        /// <param name="info">信息</param>
+        /// <param name="sign">md5后的签名</param>
+        /// <returns></returns>
+        string Sentinel(string info, string sign);
+        /// <summary>
+        /// 配置哨兵服务，种子端发送的本地配置信息
+        /// </summary>
+        /// <param name="info">信息</param>
+        /// <param name="sign">md5后的签名</param>
+        /// <returns></returns>
+        string AppConfigSentinel(string info, string sign);
+    }
+}
---
+++ Hayaaa.SeedService/ProgramSeedServer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hayaaa.SeedService
+{
+    public class ProgramSeedServer : IProgramSeedService
+    {
+        public string AppConfigSentinel(string info, string sign)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Sentinel(string info, string sign)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
